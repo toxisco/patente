8c912a81add8ba0adbcea4dfdcfbaad5e0c138e1
monossido <monossido@localhost>
2010-06-09 17:56:39 +0200

    License Stuff

--- src/com/mono/patente/Patente.java
+++ src/com/mono/patente/Patente.java
@@ -1,3 +1,18 @@
+/* Copyright (C) 2010 Lorenzo Braghetto
+ * 
+ * This file is part of BatteryDiff Widget.
+ * 
+ *   BatteryDiff Widget is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *   
+ *  BatteryDiff Widget is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+*/
+
 package com.mono.patente;
 
 import java.io.IOException;
